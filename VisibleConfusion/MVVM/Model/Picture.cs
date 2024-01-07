51da9a77b89eb81fa8117384cb297e609c83e87a
wisniax <wisnia20012@gmail.com>
2024-01-07 12:47:00 +0100

    Update proj. structure and dependency graph for PictureView

--- VisibleConfusion/MVVM/Model/Picture.cs
+++ VisibleConfusion/MVVM/Model/Picture.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace VisibleConfusion.MVVM.Model
 {
-    class CameraCapture
+    class Picture
     {
+		
     }
 }
---
+++ VisibleConfusion/MVVM/ViewModel/PictureViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisibleConfusion.Core;
+
+namespace VisibleConfusion.MVVM.ViewModel
+{
+	internal class PictureViewModel : ObservableObject
+	{
+
+	}
+}
