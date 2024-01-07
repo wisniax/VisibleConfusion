abe448a6b37abf56e9a9d04892f8ddca5b609692
wisniax <wisnia20012@gmail.com>
2024-01-07 14:39:29 +0100

    Pulled out bindings to seperate file

--- VisibleConfusion/MVVM/ViewModel/PictureViewModel.cs
+++ VisibleConfusion/MVVM/ViewModel/PictureViewModel.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Emgu.CV;
 using VisibleConfusion.Core;
 
 namespace VisibleConfusion.MVVM.ViewModel
 {
 	internal class PictureViewModel : ObservableObject
 	{
+		public BitmapSource? PictureBitmapSource { get; set; }
+		public BitmapSource? GraphBitmapSource { get; set; }
+
+		public RelayCommand? DrawByHandCommand { get; set; }
+		public RelayCommand? FromFileCommand { get; set; }
+		public RelayCommand? FromCameraCommand { get; set; }
+		public RelayCommand? CleanCommand { get; set; }
+		public RelayCommand? DoGraphCommand { get; set; }
+
+		public PictureViewModel()
+		{
+			PictureBitmapSource = new BitmapImage(new Uri("https://zoowojciechow.pl/images/lama-p-1080.jpeg"));
+			GraphBitmapSource = new BitmapImage(new Uri("https://i.gremicdn.pl/image/free/678943239052287d16ff4ae67c083de9/"));
+		}
 
 	}
 }
