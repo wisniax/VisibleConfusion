9ac6264511c72399e516f78fa02efc89d2d1b408
wisniax <wisnia20012@gmail.com>
2024-01-09 12:13:21 +0100

    Ui NumericUpDown logic fully implemented

---
+++ VisibleConfusion/MVVM/Model/PictureHandler.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.Graphics;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Xceed.Wpf.Toolkit.Core.Converters;
+
+namespace VisibleConfusion.MVVM.Model
+{
+	internal class PictureHandler
+	{
+		public class Point2D
+		{
+			public Int32 X { get; set; }
+
+			public Int32 Y { get; set; }
+
+			public Point2D() : this(0, 0)
+			{ }
+			public Point2D(Int32 x, Int32 y)
+			{
+				X = x;
+				Y = y;
+			}
+		}
+
+		public class Color
+		{
+			public byte R { get; set; }
+			public byte G { get; set; }
+			public byte B { get; set; }
+
+			public Color() : this(0, 0, 0)
+			{ }
+			public Color(Int32 r, Int32 g, Int32 b) : this(Convert.ToByte(r), Convert.ToByte(g), Convert.ToByte(b))
+			{ }
+			public Color(double r, double g, double b) : this(Convert.ToByte(r), Convert.ToByte(g), Convert.ToByte(b))
+			{ }
+			public Color(Rgb rgb) : this(Convert.ToByte(rgb.Red), Convert.ToByte(rgb.Green), Convert.ToByte(rgb.Blue))
+			{ }
+			public Color(byte r, byte g, byte b)
+			{
+				R = r;
+				G = g;
+				B = b;
+			}
+
+			public Rgb ToRgb()
+			{
+				return new Rgb(R, G, B);
+			}
+		}
+
+		public Size? FrameSize { get; set; }
+		public Size? GraphSize { get; set; }
+
+		public delegate void FrameChangedEventHandler(Image<Rgb, byte>? sender);
+		public event FrameChangedEventHandler? FrameChanged;
+		public event FrameChangedEventHandler? GraphChanged;
+
+		private Image<Rgb, byte>? _currentFrame;
+		public Image<Rgb, byte>? CurrentFrame
+		{
+			get => _currentFrame;
+			private set
+			{
+				_currentFrame?.Dispose();
+				_currentFrame = value;
+				OnFrameChanged();
+			}
+		}
+
+		private Image<Rgb, byte>? _currentGraph;
+
+		public Image<Rgb, byte>? CurrentGraph
+		{
+			get => _currentGraph;
+			private set
+			{
+				_currentGraph?.Dispose();
+				_currentGraph = value;
+				OnGraphChanged();
+			}
+		}
+
+		public PictureHandler()
+		{
+			ClearFrame();
+		}
+
+		private void OnFrameChanged()
+		{
+			FrameChanged?.Invoke(CurrentFrame);
+			CreateGraphFromFrame();
+		}
+
+		private void OnGraphChanged()
+		{
+			GraphChanged?.Invoke(CurrentGraph);
+		}
+
+		private void CreateGraphFromFrame()
+		{
+			CurrentGraph = new Image<Rgb, byte>(100, 100, new Rgb(System.Drawing.Color.Black));
+		}
+
+		public void ClearFrame()
+		{
+			CurrentFrame = new Image<Rgb, byte>(1, 1, new Rgb(System.Drawing.Color.Black));
+		}
+
+		public void GetImageFromFile(Uri uri)
+		{
+			CurrentFrame = new Image<Rgb, byte>(uri.AbsolutePath);
+		}
+
+		public Point2D GetFrameRelativePos(Point relPoint)
+		{
+			var absPoint = new Point2D(
+				Int32.Clamp(Convert.ToInt32(relPoint.X * CurrentFrame?.Width), 0, CurrentFrame?.Width - 1 ?? 0),
+				Int32.Clamp(Convert.ToInt32(relPoint.Y * CurrentFrame?.Height), 0, CurrentFrame?.Height - 1 ?? 0));
+			return absPoint;
+		}
+
+		public Color GetPixelFromPos(Point2D point)
+		{
+			var safeX = Int32.Clamp(point.X, 0, CurrentFrame?.Width - 1 ?? 0);
+			var safeY = Int32.Clamp(point.Y, 0, CurrentFrame?.Height - 1 ?? 0);
+			var rgb = CurrentFrame?[safeY, safeX] ?? new Rgb(System.Drawing.Color.Black);
+			return new Color(rgb);
+		}
+	}
+}
--- VisibleConfusion/MVVM/ViewModel/PictureViewModel.cs
+++ VisibleConfusion/MVVM/ViewModel/PictureViewModel.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Emgu.CV;
@@ -12,6 +14,8 @@ using Emgu.CV.Structure;
 using Microsoft.Win32;
 using VisibleConfusion.Core;
 using VisibleConfusion.MVVM.Model;
+using Windows.Graphics;
+using Point = Windows.Foundation.Point;
 
 namespace VisibleConfusion.MVVM.ViewModel
 {
@@ -41,20 +45,66 @@ namespace VisibleConfusion.MVVM.ViewModel
 			}
 		}
 
-		private Picture _picture;
+		private PictureHandler _pictureHandler;
 
 		public RelayCommand? DrawByHandCommand { get; set; }
 		public RelayCommand? FromFileCommand { get; set; }
 		public RelayCommand? FromCameraCommand { get; set; }
 		public RelayCommand? CleanCommand { get; set; }
 		public RelayCommand? DoGraphCommand { get; set; }
+		public RelayCommand? OnMouseLeftButtonDownOnPictureCommand { get; set; }
+		public RelayCommand? OnNumericUpDownPixelPosChangedCommand { get; set; }
+
+		private PictureHandler.Point2D _selectedPixelPos;
+
+		public PictureHandler.Point2D SelectedPixelPos
+		{
+			get => _selectedPixelPos;
+			set
+			{
+				_selectedPixelPos = value;
+				OnPropertyChanged();
+				OnSelectedPixelPosChanged();
+			}
+		}
+
+		private PictureHandler.Color _selecctedColor;
+
+		public PictureHandler.Color SelectedColor
+		{
+			get => _selecctedColor;
+			set
+			{
+				_selecctedColor = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private PictureHandler.Point2D _maxPixelPos;
+
+		public PictureHandler.Point2D MaxPixelPos
+		{
+			get => _maxPixelPos;
+			set
+			{
+				_maxPixelPos = value;
+				OnPropertyChanged();
+			}
+		}
+
 
 		public PictureViewModel()
 		{
-			_picture = new Picture();
-			//PictureBitmapSource = _picture.CurrentFrame?.Convert<Bgr, byte>().ToBitmapSource();
-			_picture.FrameChanged += (sender) => PictureBitmapSource = sender?.Convert<Bgr, byte>().ToBitmapSource();
-			_picture.GraphChanged += (sender) => GraphBitmapSource = sender?.Convert<Bgr, byte>().ToBitmapSource();
+			_pictureHandler = new PictureHandler();
+			//PictureBitmapSource = _pictureHandler.CurrentFrame?.Convert<Bgr, byte>().ToBitmapSource();
+			_pictureHandler.FrameChanged += (sender) => PictureBitmapSource = sender?.Convert<Bgr, byte>().ToBitmapSource();
+			_pictureHandler.FrameChanged += (_) => MaxPixelPos = _pictureHandler.GetFrameRelativePos(new Point(1, 1));
+			_pictureHandler.FrameChanged += (_) => OnSelectedPixelPosChanged();
+			_pictureHandler.GraphChanged += (sender) => GraphBitmapSource = sender?.Convert<Bgr, byte>().ToBitmapSource();
+
+			_selectedPixelPos = _pictureHandler.GetFrameRelativePos(new Point(0, 0));
+			_maxPixelPos = _pictureHandler.GetFrameRelativePos(new Point(1, 1));
+			_selecctedColor = _pictureHandler.GetPixelFromPos(SelectedPixelPos);
 
 			_pictureBitmapSource = new BitmapImage(new Uri("https://zoowojciechow.pl/images/lama-p-1080.jpeg"));
 			_graphBitmapSource = new BitmapImage(new Uri("https://i.gremicdn.pl/image/free/678943239052287d16ff4ae67c083de9/"));
@@ -62,8 +112,18 @@ namespace VisibleConfusion.MVVM.ViewModel
 			DrawByHandCommand = new RelayCommand((o) => throw new NotImplementedException());
 			FromFileCommand = new RelayCommand(FromFile);
 			FromCameraCommand = new RelayCommand(FromCamera);
-			CleanCommand = new RelayCommand((o) => _picture.ClearFrame());
+			CleanCommand = new RelayCommand((o) => _pictureHandler.ClearFrame());
 			DoGraphCommand = new RelayCommand((o) => throw new NotImplementedException());
+			OnMouseLeftButtonDownOnPictureCommand = new RelayCommand(MouseLeftButtonDownOnPicture);
+			OnNumericUpDownPixelPosChangedCommand = new RelayCommand((o) => OnSelectedPixelPosChanged());
+		}
+
+		private void MouseLeftButtonDownOnPicture(object obj)
+		{
+			if (obj is not MouseButtonEventArgs mouseEventArgs) return;
+			if (mouseEventArgs.OriginalSource is not Image actualImage) return;
+			var point = mouseEventArgs.GetPosition(actualImage);
+			SelectedPixelPos = _pictureHandler.GetFrameRelativePos(new Point(point.X / actualImage.ActualWidth, point.Y / actualImage.ActualHeight));
 		}
 
 		private void FromFile(object obj)
@@ -77,12 +137,17 @@ namespace VisibleConfusion.MVVM.ViewModel
 			};
 
 			if (openFileDialog.ShowDialog() != true) return;
-			_picture.GetImageFromFile(new Uri(openFileDialog.FileName));
+			_pictureHandler.GetImageFromFile(new Uri(openFileDialog.FileName));
 		}
 
 		private void FromCamera(object obj)
 		{
 			PictureBitmapSource = new BitmapImage(new Uri("http://www.quickmeme.com/img/8e/8ebf027f8f79f4b1b959341cb0a6f91bd28f6ec612dbc32c032712f8a6834a24.jpg"));
+		}
+
+		private void OnSelectedPixelPosChanged()
+		{
+			SelectedColor = _pictureHandler.GetPixelFromPos(_selectedPixelPos);
 		}
 	}
 }
