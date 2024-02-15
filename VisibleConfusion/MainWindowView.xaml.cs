35e090585e58a16915308c622b8466a52d067585
Jarosław Wierzbowski <jaroslawierzbowski2001@gmail.com>
2024-02-15 14:55:48 +0100

    Nuked Titlebar

--- VisibleConfusion/MainWindowView.xaml.cs
+++ VisibleConfusion/MainWindowView.xaml.cs
@@ -25,5 +25,43 @@ namespace VisibleConfusion
 		{
 			InitializeComponent();
 		}
-	}
+        private void OnMinimizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void OnMaximizeRestoreButtonClick(object sender, RoutedEventArgs e)
+        {
+
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+        private void RefreshMaximizeRestoreButton()
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.maximizeButton.Visibility = Visibility.Collapsed;
+                this.restoreButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                this.maximizeButton.Visibility = Visibility.Visible;
+                this.restoreButton.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void OnWindowStateChanged(object sender, EventArgs e)
+        {
+            this.RefreshMaximizeRestoreButton();
+        }
+    }
 }
